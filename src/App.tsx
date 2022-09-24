c6b9470c0037186b50856483e46cc83b678ca7fb
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-09-24 23:09:21 +0900

    zustandの標準の使い方

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Button } from "@mui/material";
+import { Stack } from "@mui/system";
+import { useState } from "react";
+import create from "zustand";
+import "./App.css";
+
+interface CountState {
+  count: number;
+  increment: () => void;
+}
+
+const useCountStore = create<CountState>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+}));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { count, increment } = useCountStore();
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Stack spacing={5}>
+        {count}
+        <Button variant="contained" onClick={() => increment()}>
+          increment
+        </Button>
+      </Stack>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
